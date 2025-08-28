faed60cb35fe139136fda3e11faaf5555c64c829
Francis Sim <fdqs215@gmail.com>
2025-08-28 10:27:47 -0700

    commit/push

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
-    print('Hello World')
-    print('CS V11: Programming Fundamentals')
+    strval = ‘Hello World’
+    course = ‘CS V11 Programming Fundamentals’
+    print(strval)
+    print(course)
+    return strval, course
 
 if __name__ == '__main__':
     main()
